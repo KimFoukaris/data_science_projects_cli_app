141dc5be5ca1897c6fee0acd265e6a6c30d331d1
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-28 22:28:25 +0000

    add bin cli and cli controller

--- lib/data_science_projects_cli_app.rb
+++ lib/data_science_projects_cli_app.rb
@@ -1,5 +1,2 @@
-require "data_science_projects_cli_app/version"
-
-module DataScienceProjectsCliApp
-  # Your code goes here...
-end
+require_relative "../lib/data_science_projects_cli_app/version"
+require_relative '../lib/data_science_projects_cli_app/cli'
---
+++ lib/data_science_projects_cli_app/cli.rb
@@ -0,0 +1,7 @@
+class DataScienceProjectsCliApp::CLI
+
+  def call
+    puts "Data Science Projects for Good Include:"
+  end
+
+end
