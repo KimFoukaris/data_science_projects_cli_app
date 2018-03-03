f893e23bf6aec43dffaf34f49a91c59f85c8efce
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-03 17:37:08 +0000

    cli controller logic

--- lib/data_science_projects_cli_app/cli.rb
+++ lib/data_science_projects_cli_app/cli.rb
@@ -10,28 +10,25 @@ class DataScienceProjectsCliApp::CLI
   def list_projects
     puts "Data Science Projects for Good Include:"
     projects = DataScienceProjectsCliApp::Project.make_projects
-    #puts projects
-    #projects.each {|project| puts "#{project.name}"}
-
-    #binding.pry
-
-    #puts "1. xxx"
-    #puts "2. yyy"
-
   end
 
   def choose_project
     project_details = DataScienceProjectsCliApp::Project.add_project_details
 
     input = nil
-    while input != "exit"
-      puts "Enter the number of the project you would like more information on:"
+    until input == "exit"
+      puts "If you would like more information on a project, enter the project number."
+      puts "If you would like to see the list again, enter 'list'."
+      puts "If not, enter 'exit'."
       #binding.pry
-      input = gets.strip.to_i
-      if input > 0 && input < project_details.length
-        puts "#{project_details[input-1].description}"
-      else
-        puts "Please enter:  the number of the project, 'list', or 'exit'"
+      input = gets.strip
+      project_num = input.to_i
+      if project_num > 0 && project_num < project_details.length
+        puts "#{project_details[project_num-1].description}"
+      elsif input == "list"
+        list_projects
+      elsif input == "exit"
+        exit
       end
     end
   end
